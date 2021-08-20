d107362792c7d4d8cc2958c1f752a6990ad6f91e
Md Yeamin <ahmedyeamin05@gmail.com>
2021-08-21 01:59:01 +0600

    js simple structure

---
+++ mac-book-pro-main/js/mac.js
@@ -0,0 +1,58 @@
+//get  ram
+const memory8BG = document.getElementById('mamory-8GB');
+const memory16BG = document.getElementById('mamory-16GB');
+//get storage
+const storage256GB = document.getElementById('storage-256GB');
+const storage512GB = document.getElementById('storage-512GB');
+const storage1TB = document.getElementById('storage-1TB');
+// Choose youe delivery option
+const freeDelivery = document.getElementById('free-delivery');
+const expressDelivery = document.getElementById('express-delivery');
+// amount quantuty
+const bestPrice = document.getElementById('best-price');
+const extraMemoryCost = document.getElementById('extra-memory-cost');
+const extraStorageCost = document.getElementById('extra-storage-cost');
+const deliveryCharge = document.getElementById('delivery-charge');
+const totalPrice = document.getElementById('total-price');
+//pomo discount
+const grandTotal = document.getElementById('grand-total');
+
+
+
+
+
+
+
+
+
+
+// get ram event 
+memory8BG.addEventListener('click', function () {
+
+})
+
+memory16BG.addEventListener('click', function () {
+
+})
+
+// get storage event 
+storage256GB.addEventListener('click', function () {
+
+})
+storage512GB.addEventListener('click', function () {
+
+})
+storage1TB.addEventListener('click', function () {
+
+
+})
+//delivery event
+freeDelivery.addEventListener('click', function () {
+    console.log('1');
+});
+expressDelivery.addEventListener('click', function () {
+    console.log('2');
+});
+
+
+
